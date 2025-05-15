23ca2d2b9b831d4f2d561b4183805aaec60699b3
jenihuerta <jeni.huerta@duocuc.cl>
2025-05-14 23:05:50 -0400

    se agrego resta

--- tarea duoc.py
+++ tarea duoc.py
@@ -2,6 +2,9 @@ num1=int(input(" ingrese su primer numero "))
 num2=int(input(" ingrese su segundo numero "))
 
 Suma_Numeros = num1 + num2 
+resta_numeros = num1 - num2 
 
 
 print("La suma de los numeros es: ", Suma_Numeros)
+print("la resta de los numeros es:", resta_numeros)
+
